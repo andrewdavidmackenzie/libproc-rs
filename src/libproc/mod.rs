2c541c5d6e53227253c50090ddcd5d91d6f39db2
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2019-10-13 12:20:49 +0200

    Only include modules that make sense on macos

--- src/libproc/mod.rs
+++ src/libproc/mod.rs
@@ -1,8 +1,12 @@
 pub mod proc_pid;
 pub mod kmesg_buffer;
+#[cfg(target_os = "macos")]
 pub mod work_queue_info;
+#[cfg(target_os = "macos")]
 pub mod thread_info;
+#[cfg(target_os = "macos")]
 pub mod task_info;
+#[cfg(target_os = "macos")]
 pub mod bsd_info;
 pub mod pid_rusage;
 pub mod file_info;
