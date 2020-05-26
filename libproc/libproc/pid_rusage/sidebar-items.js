6d8080a42236a690c62771671441182b82eefd98
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-26 19:12:38 +0000

    Deploy andrewdavidmackenzie/libproc-rs to github.com/andrewdavidmackenzie/libproc-rs.git:gh-pages

---
+++ libproc/libproc/pid_rusage/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["PidRUsageFlavor",""]],"fn":[["pidrusage","Returns the information about resources of the process that match pid passed in."]],"struct":[["RUsageInfoV0",""],["RUsageInfoV1",""],["RUsageInfoV2",""],["RUsageInfoV3",""],["RUsageInfoV4",""]],"trait":[["PIDRUsage",""]]});
