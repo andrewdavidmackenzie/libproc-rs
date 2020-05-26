5b26d1ae1b4c0ea05847a9536b6d46214a2d14d8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-26 22:02:11 +0000

    Deploy andrewdavidmackenzie/libproc-rs to github.com/andrewdavidmackenzie/libproc-rs.git:gh-pages

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["dmesg"] = {"name":"","files":["dmesg.rs"]};
-sourcesIndex["libproc"] = {"name":"","dirs":[{"name":"libproc","files":["bsd_info.rs","helpers.rs","mod.rs","proc_pid.rs","task_info.rs","thread_info.rs","work_queue_info.rs"]}],"files":["lib.rs"]};
+sourcesIndex["libproc"] = {"name":"","dirs":[{"name":"libproc","files":["bsd_info.rs","file_info.rs","helpers.rs","kmesg_buffer.rs","mod.rs","net_info.rs","pid_rusage.rs","proc_pid.rs","task_info.rs","thread_info.rs","work_queue_info.rs"]}],"files":["lib.rs"]};
 sourcesIndex["procinfo"] = {"name":"","files":["procinfo.rs"]};
 createSourceSidebar();
