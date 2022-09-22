e86b8e59d895202f22445df4fc330acebc1e3859
andrewdavidmackenzie <andrewdavidmackenzie@users.noreply.github.com>
2022-09-22 17:24:06 +0000

    Deploying to gh-pages from @ andrewdavidmackenzie/libproc-rs@3077f461884c238a61c234fa460914c2d146b84c 🚀

--- source-files.js
+++ source-files.js
@@ -1,5 +1,6 @@
-var sourcesIndex = {};
-sourcesIndex["dmesg"] = {"name":"","files":["dmesg.rs"]};
-sourcesIndex["libproc"] = {"name":"","dirs":[{"name":"libproc","files":["helpers.rs","kmesg_buffer.rs","mod.rs","pid_rusage.rs","proc_pid.rs","work_queue_info.rs"]}],"files":["lib.rs"]};
-sourcesIndex["procinfo"] = {"name":"","files":["procinfo.rs"]};
+var sourcesIndex = JSON.parse('{\
+"dmesg":["",[],["dmesg.rs"]],\
+"libproc":["",[["libproc",[],["helpers.rs","kmesg_buffer.rs","mod.rs","pid_rusage.rs","proc_pid.rs","work_queue_info.rs"]]],["lib.rs"]],\
+"procinfo":["",[],["procinfo.rs"]]\
+}');
 createSourceSidebar();
