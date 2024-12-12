e54f37aad80455a37fd9f43157f7623f4925c9ed
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-12-12 23:16:35 +0100

    Bindgen code 1 72 0 (#158)
    * Revert "Update minimum rust version to 1.82.0"
    
    This reverts commit 373a0e5fa04d35f50dccd7b8c8b75fb67ab9f44a.
    
    * Have bindgen generate code suitable for compiling with rustc 1.72.0
    
    * Avoid linux warnings
    
    * Avoid linux warnings

--- build.rs
+++ build.rs
@@ -1,22 +1,30 @@
 #[cfg(target_os = "macos")]
 fn main() {
+    use bindgen::{RustEdition, RustTarget};
     use std::env;
     use std::path::Path;
 
-    let bindings = bindgen::builder()
-        .header_contents("libproc_rs.h", "#include <libproc.h>")
-        .layout_tests(false)
-        .clang_args(&["-x", "c++", "-I", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/"])
-        .generate()
-        .expect("Failed to build libproc bindings");
+    match RustTarget::stable(72, 0) {
+        Ok(rust_target) => {
+            let bindings = bindgen::builder()
+                .header_contents("libproc_rs.h", "#include <libproc.h>")
+                .rust_target(rust_target)
+                .rust_edition(RustEdition::Edition2018)
+                .layout_tests(false)
+                .clang_args(&["-x", "c++", "-I", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/"])
+                .generate()
+                .expect("Failed to build libproc bindings");
 
-    let output_path = Path::new(&env::var("OUT_DIR")
-        .expect("OUT_DIR env var was not defined"))
-        .join("osx_libproc_bindings.rs");
+            let output_path = Path::new(&env::var("OUT_DIR")
+                .expect("OUT_DIR env var was not defined"))
+                .join("osx_libproc_bindings.rs");
 
-    bindings
-        .write_to_file(output_path)
-        .expect("Failed to write libproc bindings");
+            bindings
+                .write_to_file(output_path)
+                .expect("Failed to write libproc bindings");
+        }
+        _ => eprintln!("Error executing bindgen")
+    }
 }
 
 #[cfg(any(target_os = "linux", target_os = "redox", target_os = "android"))]
